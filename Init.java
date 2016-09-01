a539c5e9028d277193ad7a021a0d3d75038cc399
little Bei <1805698725@qq.com>
2016-09-01 09:27:42 +0800

    Add files via upload

---
+++ Init.java
@@ -0,0 +1,12 @@
+package dragon.all;
+
+/**
+ * Created by Administraor on 2016/8/29.
+ */
+public class Init {
+    public static void main(String[] args) {
+        ZongJieMiam jm=new ZongJieMiam();
+
+    }
+
+}
